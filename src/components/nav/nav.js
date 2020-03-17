e8cdaa2d001a7c83aecc59557915b38dcb6c9587
arkulnid <arkulnid@gmail.com>
2020-03-17 17:10:59 +0000

    modified:   TODO 	new file:   src/assets/expand_more.svg 	new file:   src/components/arrow/arrow.js 	new file:   src/components/arrow/arrow.pug 	new file:   src/components/arrow/arrow.scss 	modified:   src/components/button/button.pug 	deleted:    src/components/button/themes/btn_themes.scss 	new file:   src/components/nav/arrow.pug 	modified:   src/components/nav/dist/index.html 	modified:   src/components/nav/nav.js 	modified:   src/components/nav/nav.pug 	modified:   src/components/nav/nav.scss

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -1,31 +1,8 @@
-var navToggle = document.getElementsByClassName('nav__expanded')[0];
-var navMenu = document.getElementsByClassName('nav__expanded_content')[0];
-var tmp = document.getElementsByClassName('nav__item')[0];
-var isMouseDown = false;
+import "./nav.scss";
+var x = document.querySelector(".nav__expanded");
+x.onclick = function() {myEvent()};
 
-navToggle.addEventListener('click', function() {
-    this.focus();
-    navMenu.classList.toggle('nav__expanded_content-show');
-    navMenu.focus();
-});
-
-navMenu.addEventListener('mousedown', function() {
-    isMouseDown = true;  
-});
-
-navMenu.addEventListener('mouseup', function() {
-    isMouseDown = false;  
-});
-
-navMenu.addEventListener('mouseleave', function() {
-    isMouseDown = false;  
-});
-
-
-
-navMenu.addEventListener('blur', function() {
-  navMenu.classList.remove('nav__expanded_content-show');
-  //    if (!isMouseDown) {
-  //        navMenu.classList.remove('nav__expanded_content-show');
-    //    }
-}, true);
+function myEvent() {
+  document.querySelector(".nav__expanded_content").classList.toggle("nav__expanded_content-show");
+  document.querySelector(".nav__arrow").classList.toggle("nav__arrow-active");
+}
