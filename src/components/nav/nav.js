3648e4c0191a636e2cb11a23fb76d791df17b0a8
arkulnid <arkulnid@gmail.com>
2020-03-18 21:52:33 +0000

    modified:   nav.js 	modified:   nav.pug 	modified:   nav.scss

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -1,8 +1,9 @@
-var x = document.querySelector(".nav__expanded");
-x.onclick = function () {myFunction()};
+var expandedItem = document.querySelector(".nav__expanded");
+expandedItem.onclick = function () {myFunction()};
 
 function myFunction() {
   document.querySelector(".nav__expanded_content").classList.toggle("nav__expanded_content-show");
+  document.querySelector(".nav__item_arrow").classList.toggle("nav__item_arrow-active");
   dropdownClose();
 }
 
@@ -10,8 +11,10 @@ function dropdownClose() {
   window.onclick = function(e) {
     if (!e.target.matches(".nav__expanded")) {
     var myDropdown = document.querySelector(".nav__expanded_content");
+    var myDropdownArrow = document.querySelector(".nav__item_arrow");
       if (myDropdown.classList.contains("nav__expanded_content-show")) {
         myDropdown.classList.remove("nav__expanded_content-show");
+        myDropdownArrow.classList.remove("nav__item_arrow-active");
       }
     }
   }
