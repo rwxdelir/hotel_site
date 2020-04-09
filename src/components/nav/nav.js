722a96c2b99bd2676fd7346a72a7036dc8b912ac
arkulnid <arkulnid@gmail.com>
2020-04-09 20:33:29 +0000

    deleted:    arrow/arrow.js 	deleted:    arrow/arrow.pug 	deleted:    arrow/arrow.scss 	modified:   header/header.scss 	modified:   nav/nav.js 	modified:   nav/nav.pug

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -1,21 +1,24 @@
-
-var expandedItem = document.querySelector(".nav__expanded");
-expandedItem.onclick = function () {myFunction()};
-
-function myFunction() {
-  document.querySelector(".nav__expanded_content").classList.toggle("nav__expanded_content-show");
-  document.querySelector(".nav__item_arrow").classList.toggle("nav__item_arrow-active");
-  dropdownClose();
+var expandedItem = document.querySelectorAll(".nav__expanded");
+for (let i = 0; i < expandedItem.length; i++) {
+  expandedItem[i].onclick = function () {myFunction(i)};
 }
 
-function dropdownClose() {
+function myFunction(i) {
+  document.querySelectorAll(".nav__expanded_content")[i].classList.toggle("nav__expanded_content-show");
+  document.querySelectorAll(".nav__item_arrow")[i].classList.toggle("nav__item_arrow-active");
+  dropdownClose(i);
+}
+
+function dropdownClose(i) {
   window.onclick = function(e) {
     if (!e.target.matches(".nav__expanded")) {
-    var myDropdown = document.querySelector(".nav__expanded_content");
-    var myDropdownArrow = document.querySelector(".nav__item_arrow");
-      if (myDropdown.classList.contains("nav__expanded_content-show")) {
-        myDropdown.classList.remove("nav__expanded_content-show");
-        myDropdownArrow.classList.remove("nav__item_arrow-active");
+      for(let i = 0; i < expandedItem.length; i++){
+      var myDropdown = document.querySelectorAll(".nav__expanded_content")[i];
+      var myDropdownArrow = document.querySelectorAll(".nav__item_arrow")[i];
+        if (myDropdown.classList.contains("nav__expanded_content-show")) {
+            myDropdown.classList.remove("nav__expanded_content-show");
+            myDropdownArrow.classList.remove("nav__item_arrow-active");
+        }
       }
     }
   }
