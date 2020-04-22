005782b21709a95444f718424b7a2c5a5f7cf9d4
arkulnid <arkulnid@gmail.com>
2020-04-22 17:51:43 +0000

    modified:   header/header.js 	modified:   header/header.pug

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,11 +1,1 @@
-var btnLogin = document.querySelector(".header_button-login");
-var btnRegister = document.querySelector(".header_button-register");
-var header = document.querySelector(".header");
-btnLogin.onclick = function () {signin()};
 
-function signin() {
-  btnLogin.remove();
-  btnRegister.remove();
-  
-  header.className += " header_login";
-}
