391fc6af6fed07eca5d32bdca2dd09d400ab1db4
arkulnid <arkulnid@gmail.com>
2020-04-25 13:43:47 +0000

    modified:   header/header.js 	modified:   header/header.pug 	modified:   header/header.scss 	modified:   header/index.js

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,1 +1,7 @@
+var button_login = document.querySelector(".header_button-login");
+button_login.onclick = function () {openCardLogin()}; 
 
+var cardLogin = document.querySelector(".header_card-login");
+function openCardLogin() {
+  cardLogin.classList.toggle("header_card-login-show");
+}
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,3 +2,6 @@ import "./header.scss";
 import "./../nav/nav.scss";
 import "./../button/button.scss";
 import "./../logo/logo.scss";
+import "./../card/card.scss";
+import "./../textfield/textfield.scss";
+
