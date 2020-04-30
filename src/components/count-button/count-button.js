1e52c683410e5440e1498f334b27968e7c936629
arkulnid <arkulnid@gmail.com>
2020-04-30 13:26:13 +0000

    modified:   count-button/count-button.js 	deleted:    index.pug
     Changes not staged for commit:
    	modified:   ../../.gitignore

--- src/components/count-button/count-button.js
+++ src/components/count-button/count-button.js
@@ -1,22 +1,17 @@
 import "./count-button.scss";
 
-var plusBtn = document.querySelector(".count-button_plus");
-var minusBtn = document.querySelector(".count-button_minus");
-var counter = document.querySelector(".count-button_counter").innerHTML;
-plusBtn.onclick = function () {
-  if (counter < 9) {plusFunction();}
-};
-minusBtn.onclick = function () {
-  if (counter > 0) {minusFunction();}
-};
-
-function plusFunction() {
-  ++counter;
-  document.querySelector(".count-button_counter").innerHTML = counter;
+var plusBtn = document.querySelectorAll(".count-button_plus");
+var minusBtn = document.querySelectorAll(".count-button_minus");
+var countBtn = document.querySelectorAll(".count-button_counter");
+for (let i = 0; i < plusBtn.length; i++){
+  plusBtn[i].onclick = function () {
+    if (countBtn[i].innerHTML < 9) {plusFunction(i);}
+  };
+  minusBtn[i].onclick = function () {
+    if (countBtn[i].innerHTML > 0) {minusFunction(i);}
+  };
 }
 
-function minusFunction() {
-  --counter;
-  document.querySelector(".count-button_counter").innerHTML = counter;
-}
+function plusFunction(i){++countBtn[i].innerHTML;} 
+function minusFunction(i){--countBtn[i].innerHTML;}
 
