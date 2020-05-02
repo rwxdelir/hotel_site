076e06a1ad500e1cf19f7902639720c0c6752438
arkulnid <arkulnid@gmail.com>
2020-05-02 01:02:47 +0000

    modified:   count-button/count-button.js 	modified:   count-button/count-button.scss 	modified:   dropdown/dropdown.scss
     Changes not staged for commit:
    	modified:   ../../.gitignore

--- src/components/count-button/count-button.js
+++ src/components/count-button/count-button.js
@@ -2,16 +2,27 @@ import "./count-button.scss";
 
 var plusBtn = document.querySelectorAll(".count-button_plus");
 var minusBtn = document.querySelectorAll(".count-button_minus");
-var countBtn = document.querySelectorAll(".count-button_counter");
+var counterBtn = document.querySelectorAll(".count-button_counter");
+var btn = document.querySelectorAll(".count-button");
 for (let i = 0; i < plusBtn.length; i++){
   plusBtn[i].onclick = function () {
-    if (countBtn[i].innerHTML < 9) {plusFunction(i);}
+    if (counterBtn[i].innerHTML < 9) {plusFunction(i);}
   };
   minusBtn[i].onclick = function () {
-    if (countBtn[i].innerHTML > 0) {minusFunction(i);}
+    if (counterBtn[i].innerHTML > 0) {minusFunction(i);}
   };
 }
 
-function plusFunction(i){++countBtn[i].innerHTML;} 
-function minusFunction(i){--countBtn[i].innerHTML;}
+function plusFunction(i){
+  ++counterBtn[i].innerHTML; 
+  btn[i*2].style.opacity = 0.9;
+  btn[(i*2)+1].style.opacity = 0.9;
+} 
+function minusFunction(i){
+  --counterBtn[i].innerHTML;
+  if (counterBtn[i].innerHTML == 0) {
+    btn[i*2].style.opacity = 0.5;
+    btn[(i*2)+1].style.opacity = 0.5;
+  }
+}
 
