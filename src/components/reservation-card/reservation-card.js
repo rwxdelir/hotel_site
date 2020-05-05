2f79c655ca151664f089f4adb8e26fdeebf7745a
arkulnid <arkulnid@gmail.com>
2020-05-05 14:16:58 +0000

    new file:   index.pug 	new file:   reservation-card.js 	new file:   reservation-card.pug 	new file:   reservation-card.scss
     Changes not staged for commit:
    	modified:   ../../../.gitignore
    	modified:   ../card/card.scss
    	modified:   ../date-dropdown/date-dropdown.scss
    	modified:   ../dropdown/dropdown.pug
    	modified:   ../dropdown/dropdown.scss
    
     Untracked files:
    	../login-card/

---
+++ src/components/reservation-card/reservation-card.js
@@ -0,0 +1,8 @@
+import "./reservation-card.scss";
+import "./../date-dropdown/date-dropdown.scss";
+import "./../dropdown/dropdown.scss";
+import "./../button/button.scss";
+import "./../login-card/login-card.scss";
+import "./../count-button/count-button.scss";
+import "./../count-button/count-button.js";
+
