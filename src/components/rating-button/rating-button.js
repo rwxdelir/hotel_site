21b583da75100e43f16e6fbdf49ea5d90192f860
arkulnid <arkulnid@gmail.com>
2020-05-19 15:47:59 +0000

    #11 rating defined on click and can be determined in call

--- src/components/rating-button/rating-button.js
+++ src/components/rating-button/rating-button.js
@@ -1,33 +1,35 @@
-var styleElem = document.head.appendChild(document.createElement('style'));
-var ratingStar = document.getElementsByClassName("rating_star");
-var ratingDiv = document.querySelector(".rating");
+let rating = document.querySelectorAll(".rating");
+let ratingStar = document.querySelectorAll(".rating_star");
+let ratingDisabled = document.querySelectorAll(".rating_disabled");
 
-let totalRating = 5;
-let starRule = "{color: #8BA4F9; content: '★'; position: absolute;}"; 
-let starId;
-if (averageRating != false && averageRating <= totalRating) {
-   /* Direction rule in scss assigned rtl (Text direction goes from right-to-left)
-   * So we begin our cycle with total rating. totalRating-1 it`s first star, 0 - last */
-  for (let i=totalRating-1; i >= totalRating-averageRating; i--) {
-    starId = "#" + ratingStar[i].id + ":before"
-    styleElem.innerHTML += starId + starRule;
-  }
-  /* when evaluated done we disable hover of class "rating" */
-  ratingDiv.classList.add("rating_disabled");
-} else {
-  for (let j = 0; j < ratingStar.length; j++){ 
-    ratingStar[j].onclick = function(){ myFunction(j);};
-  }
-  function myFunction(average) {
-    for (let i = totalRating-1; i >= totalRating-(totalRating-average); i--) {
-      starId = "#" + ratingStar[i].id + ":before"
-      styleElem.innerHTML += starId + starRule;
+let maxRating = 5; // Maximum able rating
+
+for (var i = 0; i < rating.length; i++) {
+  /* Average rating defined */
+  if (rating[i].classList[1] == "rating_disabled") { 
+    evaluateRating(parseInt(rating[i].nextElementSibling.innerHTML), i);
+  } 
+  /* Average rating undefiend and defiend dynamically on click */
+  else {
+    for (let i = 0; i < ratingStar.length; i++) {
+      let avrg = Math.floor(maxRating-i%5);
+      let id = Math.floor(i/5);
+      ratingStar[i].onclick = function() { 
+        evaluateRating(avrg, id);
+        rating[id].classList.add("rating_disabled");
+      }
     }
-    /* if evaluated done we disable hover of class "rating" */
-    ratingDiv.classList.add("rating_disabled");
+  }
+} 
+
+/* This function assign average rating to rating form */
+function evaluateRating(avrg, ratingId) {
+  /*Direction rule in scss assigned rtl (Text direction goes from right-to-left)
+  So maxRating-1 it`s first star in the display*/
+  for (let i = maxRating-avrg; i <= 4; i++) {
+    rating[ratingId].childNodes[i].innerHTML = "★";
   }
 }
-
 
 
 
