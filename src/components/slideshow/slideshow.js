1f8a9005ba098969bc8a0ce0edcad1c748f92d00
arkulnid <arkulnid@gmail.com>
2020-05-20 16:22:42 +0000

    #11 Adds feature to create multiple review-cards in one node

--- src/components/slideshow/slideshow.js
+++ src/components/slideshow/slideshow.js
@@ -1,36 +1,47 @@
-var btnPrev = document.querySelector(".slideshow-button-prev");
-var btnNext = document.querySelector(".slideshow-button-next");
-btnPrev.onclick = function() {plusSlides(-1);}
-btnNext.onclick = function() {plusSlides(1);}
+/* Some implementation copied from https://www.w3schools.com/howto/howto_js_slideshow.asp */
+
+var btnPrev = document.querySelectorAll(".slideshow-button-prev");
+var slideshow = document.querySelectorAll(".slideshow");
+var btnNext = document.querySelectorAll(".slideshow-button-next");
+for (let i = 0; i < btnPrev.length; i++) {
+  btnPrev[i].onclick = function() {plusSlides(-1, i);}
+  btnNext[i].onclick = function() {plusSlides(1, i);}
+}
 
 var dot = document.querySelectorAll(".slideshow-dot");
 for (let i = 0; i < dot.length; i++) {
-  dot[i].onclick = function() {currentSlide(i+1);}
+  dot[i].onclick = function() {currentSlide(i+1, i);}
 }
 
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+/* Increases count indexes if we add new slideshow */
+var slideIndex = [];
+for (let i = 0; i < slideshow.length; i++) {
+  slideIndex[i] = 1;
+  showSlides(slideIndex[i], i);
 }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+function plusSlides(n, s) {
+  showSlides(slideIndex[s] += n, s);
 }
-function showSlides(n) {
+
+function currentSlide(n, s) {
+  showSlides(slideIndex = n, s);
+} 
+
+/* Shows number 'n' slide from slideshow with index 'no' */
+function showSlides(n, no) {
   var i;
-  var slides = document.getElementsByClassName("slideshow-slide");
-  var dots = document.getElementsByClassName("slideshow-dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  var slides = slideshow[no].getElementsByClassName("slideshow-slide");
+  var dots = slideshow[no].getElementsByClassName("slideshow-dot");
+  if (n > slides.length) {slideIndex[no] = 1}
+  if (n < 1) {slideIndex[no] = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" slideshow-dot-active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " slideshow-dot-active";
+  slides[slideIndex[no]-1].style.display = "block";
+  dots[slideIndex[no]-1].className += " slideshow-dot-active";
 }
 
