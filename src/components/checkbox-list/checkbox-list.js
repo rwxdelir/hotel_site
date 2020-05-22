1a2ff9e45a13ac6641c6d3e95c7f1e361e2dcfda
arkulnid <arkulnid@gmail.com>
2020-05-22 01:04:42 +0000

    * Adds toggle for dropdown list *

--- src/components/checkbox-list/checkbox-list.js
+++ src/components/checkbox-list/checkbox-list.js
@@ -1,11 +1,10 @@
 var checkboxInput = document.querySelectorAll(".checkbox-input")
 var checkboxTitle = document.querySelectorAll(".checkbox-title")
-var checkboxItem = document.querySelectorAll(".checkbox-item")
 var checkboxArrow = document.querySelectorAll(".checkbox--arrow")
 var checkmark = document.querySelectorAll(".checkbox-icon")
+var listContainer = document.querySelectorAll(".checkbox-list-container")
 
-console.log(checkboxItem )
-
+/* Keep track of checkmarks and responds on changes */
 for (let i = 0; i < checkboxInput.length; i++) {
   checkboxInput[i].addEventListener('change', function(){
     if (checkboxInput[i].checked) {
@@ -14,13 +13,23 @@ for (let i = 0; i < checkboxInput.length; i++) {
       checkmark[i].style.display = "none";
     } 
   });
-  checkboxTitle[i].addEventListener('click', function() {
-    checkboxItem[i].style.display = "block";
-  });
 }
 
-for (let i = 0; i < checkboxTitle.length; i++) {
-  checkboxTitle[i].addEventListener('click', function() {
-    checkboxItem[i].style.display = "block";
-  });
+/* Adds onclick into titles and assign container style to block */
+for (let i = 0; i < listContainer.length; i++) {
+  checkboxTitle[i].onclick = function() {listToggle(i)};
+  listContainer[i].style.display = "block";
+}
+
+function listToggle(i) {
+  if (listContainer[i].style.display == "block") {
+    listContainer[i].style.display = "none";
+    checkboxArrow[i].style.transform = "rotate(180deg)";
+    checkboxArrow[i].style.top = "5";
+
+  } else {
+    listContainer[i].style.display = "block";
+    checkboxArrow[i].style.transform = "rotate(0deg)";
+    checkboxArrow[i].style.top = "-2.6";
+  }
 }
