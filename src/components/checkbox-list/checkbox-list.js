1d3a0f8a66e058ae52f7ec9f7fd22951d5d359a1
arkulnid <arkulnid@gmail.com>
2020-05-22 14:02:32 +0000

    * checkbox-list devided into buttons and list *

--- src/components/checkbox-list/checkbox-list.js
+++ src/components/checkbox-list/checkbox-list.js
@@ -1,19 +1,6 @@
-var checkboxInput = document.querySelectorAll(".checkbox-input")
-var checkboxTitle = document.querySelectorAll(".checkbox-title")
-var checkboxArrow = document.querySelectorAll(".checkbox--arrow")
-var checkmark = document.querySelectorAll(".checkbox-icon")
+var checkboxTitle = document.querySelectorAll(".checkbox-list-title")
+var checkboxArrow = document.querySelectorAll(".checkbox-list--arrow")
 var listContainer = document.querySelectorAll(".checkbox-list-container")
-
-/* Keep track of checkmarks and responds on changes */
-for (let i = 0; i < checkboxInput.length; i++) {
-  checkboxInput[i].addEventListener('change', function(){
-    if (checkboxInput[i].checked) {
-      checkmark[i].style.display = "block";
-    } else {
-      checkmark[i].style.display = "none";
-    } 
-  });
-}
 
 /* Adds onclick into titles and assign container style to block */
 for (let i = 0; i < listContainer.length; i++) {
