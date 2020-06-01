1046e920269251bb47f0b97c055b08f2a62f2143
arkulnid <arkulnid@gmail.com>
2020-06-01 15:38:27 +0000

    * Append checked parametr for like *

--- src/components/like-button/like-button.js
+++ src/components/like-button/like-button.js
@@ -3,20 +3,23 @@ let likeLabel = document.querySelectorAll(".like-icon");
 let likeCounter = document.querySelectorAll(".like-counter");
 let likeCondition = document.querySelectorAll(".like-condition");
 
+
+
 for (let i = 0; i < likeInput.length; i++) {
   likeLabel[i].innerHTML = "favorite_border";
 }
+
 for (let i = 0; i < likeInput.length; i++) {
+  /* like input already checked */
+  if (likeInput[i].checked) {
+    likeColored(i);
+  }
+  /* Like input doesn`t checked yet but can be */
   likeInput[i].addEventListener('change', function(){
     if (likeInput[i].checked) {
-      /* Change icon */
-      likeLabel[i].innerHTML = "favorite";
-      likeLabel[i].classList.add("like-icon-favorite")
-      /* Disable like btn */
-      likeInput[i].disabled = true; 
-      /* Increment and digit color */
+      likeColored(i);
+      /* Increment like counter */
       ++likeCounter[i].innerHTML; 
-      likeCounter[i].style.color = "#BC9CFF"
     } else {
       likeLabel[i].innerHTML = "favorite_border";
       likeLabel[i].classList.remove("like-icon-favorite")
@@ -24,3 +27,12 @@ for (let i = 0; i < likeInput.length; i++) {
   });
 }
 
+/* Make like input checked */
+function likeColored(i) {
+  /* Change icon */
+  likeLabel[i].innerHTML = "favorite";
+  likeLabel[i].classList.add("like-icon-favorite")
+  /* Disable like btn */
+  likeInput[i].disabled = true;  
+  likeCounter[i].style.color = "#BC9CFF"
+}
