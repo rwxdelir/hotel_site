05d0edd835d81c81a4b0d253d0b5dacad93f0a52
arkulnid <arkulnid@gmail.com>
2020-06-01 15:48:25 +0000

    * Appends right position and rotation deggress for arrows *

--- src/components/checkbox-list/checkbox-list.js
+++ src/components/checkbox-list/checkbox-list.js
@@ -1,6 +1,6 @@
 var checkboxTitle = document.querySelectorAll(".checkbox-list-title")
 var checkboxArrow = document.querySelectorAll(".checkbox-list--arrow")
-var listContainer = document.querySelectorAll(".checkbox-list-container")
+var listContainer = document.querySelectorAll(".checkbox-expanded-content")
 
 /* Adds onclick into titles and assign container style to block */
 for (let i = 0; i < listContainer.length; i++) {
@@ -11,12 +11,13 @@ for (let i = 0; i < listContainer.length; i++) {
 function listToggle(i) {
   if (listContainer[i].style.display == "block") {
     listContainer[i].style.display = "none";
-    checkboxArrow[i].style.transform = "rotate(180deg)";
-    checkboxArrow[i].style.top = "5";
+    checkboxArrow[i].style.transform = "rotate(0deg)";
+    checkboxArrow[i].style.top = "-2.6";
 
   } else {
     listContainer[i].style.display = "block";
-    checkboxArrow[i].style.transform = "rotate(0deg)";
-    checkboxArrow[i].style.top = "-2.6";
+    checkboxArrow[i].style.transform = "rotate(-180deg)";
+    checkboxArrow[i].style.top = "4px";
   }
 }
+
