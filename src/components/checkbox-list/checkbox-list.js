ba3cca6c25c76e5d4bb5f5250bccb6eba4cd6c5e
arkulnid <arkulnid@gmail.com>
2020-06-22 20:15:51 +0000

    * Change arrow in checkbox-list on material icons *

--- src/components/checkbox-list/checkbox-list.js
+++ src/components/checkbox-list/checkbox-list.js
@@ -5,19 +5,16 @@ var listContainer = document.querySelectorAll(".checkbox-expanded-content")
 /* Adds onclick into titles and assign container style to block */
 for (let i = 0; i < listContainer.length; i++) {
   checkboxTitle[i].onclick = function() {listToggle(i)};
-  listContainer[i].style.display = "block";
 }
 
 function listToggle(i) {
   if (listContainer[i].style.display == "block") {
     listContainer[i].style.display = "none";
-    checkboxArrow[i].style.transform = "rotate(0deg)";
-    checkboxArrow[i].style.top = "-2.6";
+    checkboxArrow[i].innerHTML = "keyboard_arrow_down"
 
   } else {
     listContainer[i].style.display = "block";
-    checkboxArrow[i].style.transform = "rotate(-180deg)";
-    checkboxArrow[i].style.top = "4px";
+    checkboxArrow[i].innerHTML = "keyboard_arrow_up"
   }
 }
 
