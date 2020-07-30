4e0e9e4b6cf6cfee70bb68b8c818fa5f09f502b5
arkulnid <arkulnid@gmail.com>
2020-07-30 18:35:03 +0000

    Refactor: header & menu

--- src/components/header/header.js
+++
@@ -1,7 +0,0 @@
-var button_login = document.querySelector(".header_button-login");
-button_login.onclick = function () {openCardLogin()}; 
-
-var cardLogin = document.querySelector(".header_card-login");
-function openCardLogin() {
-  cardLogin.classList.toggle("header_card-login-show");
-}
