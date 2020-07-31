ed676f2934d122b8591a37da8bf94368111c61d6
arkulnid <arkulnid@gmail.com>
2020-07-31 19:36:41 +0000

    Refactor: textfield mixin

--- src/components/textfield/textfield.js
+++ src/components/textfield/textfield.js
@@ -1,11 +1,1 @@
 import "./textfield.scss";
-import "./../card/card.scss";
-import "./../button/button.scss";
-import "./../arrow/arrow.scss";
-import "./../radio-button/radio-button.scss";
-import "./../toggle-button/toggle-button.scss";
-import "./../date-dropdown/date-dropdown.scss";
-import "./../dropdown/dropdown.scss";
-import "./../count-button/count-button.scss";
-import "./../arrow/arrow.scss";
-
